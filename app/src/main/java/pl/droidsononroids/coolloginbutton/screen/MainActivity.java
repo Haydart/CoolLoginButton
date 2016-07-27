3ad0edfb431e5d60b0dfc6b223c0afed2e0f46b7
Haydart <makowiecki.radoslaw@gmail.com>
2016-07-27 11:17:26 +0200

    Inflate button & add component communication

--- app/src/main/java/pl/droidsononroids/coolloginbutton/screen/MainActivity.java
+++ app/src/main/java/pl/droidsononroids/coolloginbutton/screen/MainActivity.java
@@ -3,16 +3,21 @@ package pl.droidsononroids.coolloginbutton.screen;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import pl.droidsononroids.coolloginbutton.R;
 import pl.droidsononroids.coolloginbutton.api.LoginManager;
+import pl.droidsononroids.coolloginbutton.view.LoginButton;
 
 public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.toolbar) Toolbar mToolbar;
     @BindView(R.id.email_edit_text) EditText mEmailEditText;
+    @BindView(R.id.loginButton) LoginButton loginButton;
     private LoginManager mLoginManager;
 
     @Override
@@ -25,12 +30,21 @@ public class MainActivity extends AppCompatActivity {
         mLoginManager.setLoginListener(new LoginManager.LoginListener() {
             @Override
             public void loginSuccess() {
-
+                Toast.makeText(MainActivity.this, "SUCCESSFUL LOGIN", Toast.LENGTH_SHORT).show();
+                loginButton.displaySuccessAnimation();
             }
 
             @Override
             public void loginFailure() {
-                //no-op
+                Toast.makeText(MainActivity.this, "LOGIN FAILURE", Toast.LENGTH_SHORT).show();
+                loginButton.displayFailureAnimation();
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mLoginManager.performLogin(mEmailEditText.getText().toString().trim());
             }
         });
     }
--- app/src/main/java/pl/droidsononroids/coolloginbutton/view/LoginButton.java
+++ app/src/main/java/pl/droidsononroids/coolloginbutton/view/LoginButton.java
@@ -1,4 +1,28 @@
 package pl.droidsononroids.coolloginbutton.view;
 
-public class LoginButton {
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import pl.droidsononroids.coolloginbutton.R;
+
+public class LoginButton extends FrameLayout{
+    public LoginButton(Context context) {
+        this(context, null);
+    }
+
+    public LoginButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        LayoutInflater.from(context).inflate(R.layout.login_button_layout, this, true);
+    }
+
+    public void displaySuccessAnimation(){
+
+    }
+
+    public void displayFailureAnimation(){
+
+    }
 }
